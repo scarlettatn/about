dc0ae2b21abef2b196b864ca0257598fec35cf5c
Scarlett Ah-Thon <scarlettahthon@gmail.com>
2023-11-23 21:24:49 +0000

    config

--- my-app/api/server.js
+++ my-app/api/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req, res) {
-  res.send("It's working!")
+  res.send({"name": "Jane Doe"}) // Should be json format
 })
 
 app.listen(3000, () => {
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and ok doki.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: ""
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000")
+      .then(res => res.json())
+      .then(data => this.setState({ name: data.name }))
+  }
+
+  render() {
+    return (
+      <h1>Hello {this.state.name}!</h1>
+    )
+  }
 }
 
-export default App;
+export default App
