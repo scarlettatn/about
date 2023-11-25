fdf94bbc26c877ebb203fad6d9b087510e8f2b3f
Scarlett Ah-Thon <scarlettahthon@gmail.com>
2023-11-25 13:50:23 +0000

    fix CORS issue

--- my-app/api/server.js
+++ my-app/api/server.js
@@ -11,3 +11,17 @@ app.listen(3000, () => {
 })
 
 app.use(cors())
+
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
